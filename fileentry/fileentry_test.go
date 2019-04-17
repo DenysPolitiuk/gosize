9a0a2b58f57b4c01df673f343de70fbb87a6d693
DenysPolitiuk <denys.politiuk@protonmail.com>
2019-04-16 23:04:10 -0400

    More work on tests

--- fileentry/fileentry_test.go
+++ fileentry/fileentry_test.go
@@ -9,13 +9,26 @@ import (
 	"testing"
 	"time"
 )
+import _ "io/ioutil"
+
+// TODO: use for going through folder inner files
+
+const (
+	minFolders     int    = 25
+	maxFolders     int    = 50
+	minFiles       int    = 1
+	maxFiles       int    = 20
+	minFileSizeItr int    = 1
+	maxFileSizeItr int    = 1000
+	fileContent    string = "0123456789"
+)
 
 func testFolderSetup(dir string) ([]string, error) {
 	err := os.Mkdir(dir, os.ModePerm)
 	if err != nil {
 		return []string{}, err
 	}
-	allFiles := make([]string, 1, 50)
+	allFiles := make([]string, 1, maxFolders*maxFiles)
 	f, err := testFileSetup(dir)
 	if err != nil {
 		os.RemoveAll(dir)
@@ -23,7 +36,7 @@ func testFolderSetup(dir string) ([]string, error) {
 	}
 	// set up and add one file for fail tests
 	allFiles[0] = filepath.Join(dir, f)
-	for i := 0; i < randomNumber(1, 20); i++ {
+	for i := 0; i < randomNumber(minFolders, maxFolders); i++ {
 		folderName := strconv.Itoa(int(time.Now().UnixNano()))
 		newFolder := filepath.Join(dir, folderName)
 		err = os.Mkdir(newFolder, os.ModePerm)
@@ -31,12 +44,13 @@ func testFolderSetup(dir string) ([]string, error) {
 			os.RemoveAll(dir)
 			return []string{}, err
 		}
-		for j := 0; j < randomNumber(1, 20); j++ {
-			_, err = testFileSetup(newFolder)
+		for j := 0; j < randomNumber(minFiles, maxFiles); j++ {
+			aFile, err := testFileSetup(newFolder)
 			if err != nil {
 				os.RemoveAll(dir)
 				return []string{}, err
 			}
+			allFiles = append(allFiles, aFile)
 		}
 	}
 	return allFiles, nil
@@ -44,14 +58,14 @@ func testFolderSetup(dir string) ([]string, error) {
 
 func testFileSetup(dir string) (string, error) {
 	unixTime := time.Now().UnixNano()
-	fileName := fmt.Sprintf("%v.txt", unixTime)
+	fileName := strconv.FormatInt(unixTime, 10) //fmt.Sprintf("%v", unixTime)
 	f, err := os.Create(filepath.Join(dir, fileName))
 	if err != nil {
 		return "", err
 	}
 	defer f.Close()
-	for i := 0; i < randomNumber(1, 100); i++ {
-		_, err := f.WriteString("0123456789")
+	for i := 0; i < randomNumber(minFileSizeItr, maxFileSizeItr); i++ {
+		_, err := f.WriteString(fileContent)
 		if err != nil {
 			return "", err
 		}
@@ -63,12 +77,28 @@ func randomNumber(min int, max int) int {
 	return rand.Intn(max-min) + min
 }
 
+func getFolderSize(folder string) (int64, error) {
+	var totalSize int64
+	err := filepath.Walk(folder, func(_ string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			totalSize += info.Size()
+		}
+		return err
+	})
+	return totalSize, err
+}
+
 var allFiles []string
+
+const testFolderName = "test_target"
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
 	var err error
-	allFiles, err = testFolderSetup("test_target")
+	allFiles, err = testFolderSetup(testFolderName)
 	if err != nil {
 		// if error try to remove existing test_target and try again
 		os.RemoveAll("test_target")
@@ -95,19 +125,45 @@ func TestNewFileEntry(t *testing.T) {
 		case true:
 		case false:
 			if err == nil {
-				t.Errorf("Parser(%v) returned nil instead of %v", c.in, got)
+				t.Errorf("NewFileEntry(%v) returned nil instead of %v", c.in, got)
 			}
 		}
 	}
 }
 
 func TestFillContent(t *testing.T) {
-	cases := []struct {
-		in    string
-		out   *FileEntry
-		valid bool
-	}{}
-	for _, _ = range cases {
+	totalSize, err := getFolderSize(testFolderName)
+	if err != nil {
+		t.Error("Unable to getFolderSize", err)
+		return
 	}
-	t.Errorf("Not implemented")
+	fe, err := NewFileEntry(testFolderName)
+	if err != nil {
+		t.Error("Unable to NewFileEntry", err)
+		return
+	}
+	err = fe.FillContent()
+	if err != nil {
+		t.Error("Error during FillContent", err)
+		return
+	}
+	if float64(fe.Size) != float64(totalSize) {
+		t.Errorf("Total size for NewFileEntry is %v but it is %v for test result", float64(fe.Size), totalSize)
+	}
+	// for future tests
+	// files, err := ioutil.ReadDir(testFolderName)
+	// if err != nil {
+	// t.Error("Unable to ReadDir", err)
+	// return
+	// }
+	// for _, f := range files {
+	// if !f.IsDir() {
+	// continue
+	// }
+	// fSize, err := getFolderSize(f.Name())
+	// if err != nil {
+	// t.Error("Unable to getFolderSize", err)
+	// return
+	// }
+	// }
 }
